7b21bf577ec7a166ff451d7a01c8420c170448c5
joaco <joaquin.garciaq@correo.ucu.edu.uy>
2025-08-17 18:39:45 -0300

    Agregue feature addition

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
-// Esta clase implementa la operación Suma
+
 public class Addition
 {
     public static int Add(int a, int b)
@@ -11,58 +11,3 @@ public class Addition
     }
 }
 
-
-
-
-
-
-
-
-public class Substraction
-{
-    public static int Substract(int a, int b)
-    {
-        return a - b;
-    }
-}
-// Esta clase implementa la operación division
-public classs Division
-{
-    public static double Divide(int a, int b)
-    {
-        return (double)a / b;
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-public class Division
-{
-    public static double Divide(int a, int b)
-    {
-        return (double)a / b;
-    }
-}
